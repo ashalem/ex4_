9fecd3a103f408e16f71bad0a7f2d320bfd5c69c
ashalem <arielshalem1@gmail.com>
2022-06-18 23:14:55 +0300

    feature: Changed merchant code to cpp style (card_b)

--- Merchant.cpp
+++ Merchant.cpp
@@ -1,66 +1,73 @@
 #include <string>
+#include <memory>
 
 #include "utilities.h"
 #include "Player.h"
 #include "Card.h"
 #include "Merchant.h"
 
+Merchant::Action::Action(int cost) : m_cost(cost) {}
+
+int Merchant::Action::getCost() { 
+    return this->m_cost;
+}
+
+Merchant::HealthPotion::HealthPotion() : Merchant::Action(0) {}
+
+void Merchant::HealthPotion::doBenefit(Player &player) const {}
+
+Merchant::HealthPotion::HealthPotion() : Merchant::Action(5) {}
+
+void Merchant::HealthPotion::doBenefit(Player &player)  const {
+    player.heal(1);
+}
+
+Merchant::ForceBoost::ForceBoost() : Merchant::Action(10) {}
+
+void Merchant::ForceBoost::doBenefit(Player &player)  const{
+    player.buff(1);
+}
+
 Merchant::Merchant() : Card("Merchant") {}
 
-static int getUserAction() {
+static std::unique_ptr<Merchant::Action> Merchant::getUserAction() {
     std::string userInput;
     std::getline(std::cin, userInput);
 
-    int playerChoice;
-    bool hasGottenValidInput = false;
-    while (!hasGottenValidInput) {
+    while (true) {
+        int playerChoice;
         try {
             playerChoice = std::stoi(userInput.c_str());
         } catch (std::invalid_argument) {
             printInvalidInput();
             continue;
         }
-        hasGottenValidInput = true;
-    }
-}
 
-static void applyHealPotion(Player &player) {
-    player.heal(1);
-}
-static void applyForceBoost(Player &player) {
-    player.buff(1);
+        switch (playerChoice) {
+        case static_cast<int>(Merchant::ActionEnum::Nothing):
+            return std::unique_ptr<Merchant::NoAction>;
+        case static_cast<int>(Merchant::ActionEnum::HealthBoost):
+            return std::unique_ptr<Merchant::HealthPotion>;
+        case static_cast<int>(Merchant::ActionEnum::ForceBoost):
+            return std::unique_ptr<Merchant::ForceBoost>;
+        default:
+            printInvalidInput();
+            continue;
+        }
+    }
 }
 
 void Merchant::applyEncounter(Player &player) const  {
 
     printMerchantInitialMessageForInteractiveEncounter(std::cout, player.getName(), player.getCoins());
 
-    int playerChoice = getUserAction();
+    std::unique_ptr<Merchant::Action> merchantAction = getUserAction();
 
-    int cost = 0;
-    switch (playerChoice) {
-    case static_cast<int>(Merchant::Action::Nothing):
-        break;
-    case static_cast<int>(Merchant::Action::HealthBoost):
-        cost = healCost;
-        if (Player.pay(cost)) {
-            applyHealPotion(player)
-        } else {
-            printMerchantInsufficientCoins(std::cout);
-        }
-        break;
-    case static_cast<int>(Merchant::Action::ForceBoost):
-        cost = forceCost;
-        if (Player.pay(cost)) {
-            applyForceBoost(player)
-        } else {
-            printMerchantInsufficientCoins(std::cout);
-        }
-        break;
-    default:
-        printInvalidInput();
-        break;
+    if (Player.pay(merchantAction->getCost())) {
+        merchantAction->doBenefit(player)
+    } else {
+        printMerchantInsufficientCoins(std::cout);
     }
     
-    printMerchantSummary(std::cout, player.getName(), playerChoice, cost);
+    printMerchantSummary(std::cout, player.getName(), playerChoice, merchantAction->getCost());
 }
--- Merchant.h
+++ Merchant.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <iostream>
+#include <memory>
 
 #include "Player.h"
 #include "Card.h"
@@ -35,12 +36,58 @@ public:
     Merchant& operator=(const Merchant& other) = default;
 
 private:
-    enum class Action {Nothing = 0, HealthBoost, ForceBoost};
-    static const int healCost = 5;
-    static const int forceCost = 10;
+    enum class ActionEnum {Nothing = 0, HealthBoost, ForceBoost};
+    class Action;
+    class NoAction: public Action;
+    class HealthPotion: public Action;
+    class ForceBoost: public Action;
 
-    static Merchant::Action getUserAction();
+    static std::unique_ptr<Merchant::Action> getUserAction();
 };
+
+class Merchant::Action {
+public:
+    Action(int cost);
+    virtual coid doBenefit(Player &player) const  = 0;
+
+    virtual ~Action() = default;
+    Action(const Action&) = default;
+    operator=(const Action&) = default;
+
+    int getCost() const;
+private:
+    int m_cost;
+}
+
+class Merchant::NoAction : public Merchant::Action {
+public:
+    HealthPotion();
+    void doBenefit(Player &player) const override;
+
+    virtual ~NoAction() = default;
+    NoAction(const NoAction&) = default;
+    operator=(const NoAction&) = default;
+}
+
+class Merchant::HealthPotion : public Merchant::Action {
+public:
+    HealthPotion();
+    void doBenefit(Player &player) const override;
+
+    virtual ~HealthPotion() = default;
+    HealthPotion(const HealthPotion&) = default;
+    operator=(const HealthPotion&) = default;
+}
+
+class Merchant::ForceBoost : public Merchant::Action {
+public:
+    ForceBoost();
+    void doBenefit(Player &player) const override;
+
+    virtual ~ForceBoost() = default;
+    ForceBoost(const ForceBoost&) = default;
+    operator=(const ForceBoost&) = default;
+}
 
 
 #endif //EX4_Merchant_H
