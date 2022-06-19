cf60da21312d1b0d41273bca03484be03b85100d
ashalem <arielshalem1@gmail.com>
2022-06-19 11:51:18 +0300

    feature: Merchant code pass compilation (merchant_fixes_b)

--- Cards/Merchant.cpp
+++ Cards/Merchant.cpp
@@ -6,23 +6,27 @@
 #include "Card.h"
 #include "Merchant.h"
 
-Merchant::Action::Action(int cost) : m_cost(cost) {}
+Merchant::Action::Action(int cost, Merchant::ActionEnum typeEnum) : m_cost(cost), m_typeEnum(typeEnum) {}
 
-int Merchant::Action::getCost() { 
+int Merchant::Action::getCost() const { 
     return this->m_cost;
 }
 
-Merchant::HealthPotion::HealthPotion() : Merchant::Action(0) {}
+Merchant::ActionEnum Merchant::Action::getTypeEnum() const { 
+    return this->m_typeEnum;
+}
 
-void Merchant::HealthPotion::doBenefit(Player &player) const {}
+Merchant::NoAction::NoAction() : Merchant::Action(0, Merchant::ActionEnum::Nothing) {}
 
-Merchant::HealthPotion::HealthPotion() : Merchant::Action(5) {}
+void Merchant::NoAction::doBenefit(Player &player) const {}
+
+Merchant::HealthPotion::HealthPotion() : Merchant::Action(5, Merchant::ActionEnum::HealthBoost) {}
 
 void Merchant::HealthPotion::doBenefit(Player &player)  const {
     player.heal(1);
 }
 
-Merchant::ForceBoost::ForceBoost() : Merchant::Action(10) {}
+Merchant::ForceBoost::ForceBoost() : Merchant::Action(10, Merchant::ActionEnum::ForceBoost) {}
 
 void Merchant::ForceBoost::doBenefit(Player &player)  const{
     player.buff(1);
@@ -30,7 +34,7 @@ void Merchant::ForceBoost::doBenefit(Player &player)  const{
 
 Merchant::Merchant() : Card("Merchant") {}
 
-static std::unique_ptr<Merchant::Action> Merchant::getUserAction() {
+std::unique_ptr<Merchant::Action> Merchant::getUserAction() {
     std::string userInput;
     std::getline(std::cin, userInput);
 
@@ -45,13 +49,14 @@ static std::unique_ptr<Merchant::Action> Merchant::getUserAction() {
 
         switch (playerChoice) {
         case static_cast<int>(Merchant::ActionEnum::Nothing):
-            return std::unique_ptr<Merchant::NoAction>;
+            return std::unique_ptr<Merchant::NoAction>();
         case static_cast<int>(Merchant::ActionEnum::HealthBoost):
-            return std::unique_ptr<Merchant::HealthPotion>;
+            return std::unique_ptr<Merchant::HealthPotion>();
         case static_cast<int>(Merchant::ActionEnum::ForceBoost):
-            return std::unique_ptr<Merchant::ForceBoost>;
+            return std::unique_ptr<Merchant::ForceBoost>();
         default:
             printInvalidInput();
+            std::getline(std::cin, userInput);
             continue;
         }
     }
@@ -63,11 +68,14 @@ void Merchant::applyEncounter(Player &player) const  {
 
     std::unique_ptr<Merchant::Action> merchantAction = getUserAction();
 
-    if (Player.pay(merchantAction->getCost())) {
-        merchantAction->doBenefit(player)
+    if (player.pay(merchantAction->getCost())) {
+        merchantAction->doBenefit(player);
     } else {
         printMerchantInsufficientCoins(std::cout);
     }
     
-    printMerchantSummary(std::cout, player.getName(), playerChoice, merchantAction->getCost());
+    printMerchantSummary(std::cout,
+                        player.getName(),
+                        static_cast<int>(merchantAction->getTypeEnum()), 
+                        merchantAction->getCost());
 }
